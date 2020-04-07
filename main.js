024fdde10210cf2f08537c079b4c19eaa5439ce1
juliastefanoni <juliastefanonidev@gmail.com>
2020-04-07 19:32:12 -0300

    jogo finalizado

--- main.js
+++ main.js
@@ -48,6 +48,13 @@ function startGame() {
   if(snake[0].y > 15 * box && direction === "down") snake[0].y = 0;
   if(snake[0].y < 0 && direction === "up") snake[0].y = 16 * box;
 
+  for(i = 1; i < snake.length; i++) {
+    if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+      clearInterval(game);
+      alert("Você perdeu :( ");
+      location.reload();
+    }
+  }
   createBackground();
   createSnake();
   createFood();
