8c29f65cc7c0db2467f8999ab62514a5ed9ba237
juliastefanoni <juliastefanonidev@gmail.com>
2020-04-07 19:24:52 -0300

    aleatoriedade da comida e tamanho da cobrinha

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function createSnake() {
 }
 
 function createFood() {
-  context.fillStyle = "#C20E2C";
+  context.fillStyle = "#C2402C";
   context.fillRect(food.x, food.y, box, box);
 }
 
@@ -60,7 +60,14 @@ function startGame() {
   if(direction === 'up') snakeY -= box;
   if(direction === 'down') snakeY += box;
 
-  snake.pop();
+  if(snakeX !== food.x || snakeY !== food.y){
+    snake.pop();
+  }
+  else{
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
+
   let newHead = {
     x: snakeX, 
     y: snakeY
