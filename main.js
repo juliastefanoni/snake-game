de5a1a902f426b13acbe823232420ae7f8621c67
juliastefanoni <juliastefanonidev@gmail.com>
2020-04-07 18:44:46 -0300

    criar cobrinha

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-(function(){
-  let canvas = document.querySelector('[data-js="snake"]');
+  let canvas = document.getElementById("snake");
   let context = canvas.getContext("2d");
   let box = 32;
+  let snake = [];
 
-  function criarBackground(){
+  snake[0] = {
+    x: 8*box,
+    y: 8*box
+  };
+  
+  function createBackground() {
     context.fillStyle = "#322E87";
     context.fillRect(0, 0, 16 * box, 16 * box);
   }
 
-  criarBackground();
+  function createSnake (){
+    for(i = 0; i < snake.length; i++){
+      context.fillStyle = "#00FFFF";
+      context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+  }
 
-})();
+  createBackground();
+  createSnake();
